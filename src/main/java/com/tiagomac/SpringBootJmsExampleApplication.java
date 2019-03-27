7e7353316c43b45ddcdb54afb97b035e4010366c
Cunha <tiago.cunha@capgemini.com>
2019-03-27 16:40:10 -0300

    first commit

---
+++ src/main/java/com/tiagomac/SpringBootJmsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tiagomac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsExampleApplication.class, args);
+	}
+
+}
